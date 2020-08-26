a8d3db78eab3efaa4de0b1e3ffe11f031cec6e62
Lars Erlandson <Erlandson@Larss-iMac.home>
2020-08-25 21:59:44 -0500

    removed logo from app and started new branch

--- src/client/App.js
+++ src/client/App.js
@@ -17,7 +17,7 @@ export default class App extends Component {
       <div>
         Test
         {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+      
       </div>
     );
   }
