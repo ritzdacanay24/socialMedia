f83603159f26a2197660a194ca05eb75f285eb2a
Damon Schulz <damon.schulz@gmail.com>
2020-08-31 21:18:00 -0500

    added button to log data

--- src/client/Components/ProfilePage.js
+++ src/client/Components/ProfilePage.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, useCallback} from 'react';
-import {Container, Col, Row, Button} from 'reactstrap';
+import {Form, Col, Row, Button} from 'reactstrap';
 import axios from 'axios';
 
 
@@ -27,13 +27,30 @@ function ProfilePage(props) {
     }, [fetchData])
 
 
+    showData((event) => {
+        console.log({responseData})
+    })
+
     return (
         
         <div>
             <Row id="profileCon">
                 <Col><h1>{responseData.firstName}</h1></Col>
                 <Col>
-            <Button>View All Friends</Button>
+            <Button onClick={showData()}>log data</Button>
+            <Form>
+                <ul>
+                    <li>
+                        {responseData.firstName}
+                    </li>
+                    <li>
+                        {responseData.lastName}
+                    </li>
+                    <li>
+                        {responseData.email}
+                    </li>
+                </ul>
+            </Form>
             </Col>
             </Row>
         </div>
