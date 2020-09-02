e0cf6e3454c51f83b26ed9faaf49525ffa1c09f7
Ritz Dacanay <ritzdacanay24@yahoo.com>
2020-09-02 00:06:59 -0700

    add like and dislike function

--- src/client/Components/viewPosts/ViewPosts.js
+++ src/client/Components/viewPosts/ViewPosts.js
@@ -30,29 +30,49 @@ class ViewPosts extends Component {
     })
   }
 
+  likeDislikeOnHandler = (action, post) => {
+    
+    if(action == 'like') post.likes++;
+    if(action == 'dislike') post.dislikes++;
+    console.log(post)
+
+    Axios.put('http://localhost:5000/api/posts/likeDislike', {
+        "dislikes": post.dislikes,
+        "likes": post.likes,
+        "id": post._id
+    })
+        .then(res => {
+            this.getPosts()
+
+        }, function (err) {
+
+            console.log(err)
+        })
+}
+
   setPosts = () => {
     if(!this.props.friendPosts.length){
       return "No Posts"
     }
     return this.props.friendPosts.length && this.props.friendPosts.map((post, index) =>
-      <Card key={index}>
+      <Card key={index} className="text-dark margin-bottom-10">
         <CardBody>
           <Media>
             <Media left top href="#">
-              <Media object src="src/client/assets/images/default.jpg" alt="test" style={{ width: "40px", borderRadius: "100px" }} />
+              <Media className="light-shadow" object src={post.profileImage} alt="test" style={{ maxWidth: "80px",maxHeight: "80px", borderRadius: "100px", overflow: "hidden" }} />
             </Media>
             <Media body>
               <Media>
-                <div style={{ textAlign: "left", marginLeft: "30px" }}>
+                <div style={{ textAlign: "left", marginLeft: "50px" }}>
                   <b>{post.firstName} {post.lastName}</b> <small> {post.createdDate}</small>
-                  <p style={{ fontWeight: "500" }}>{post.comment}</p>
+                  <h4 style={{ fontWeight: "500" }}>{post.comment}</h4>
                 </div>
               </Media>
-              <div style={{ textAlign: "left", marginLeft: "30px" }}>
-                <span onClick={() => this.props.likeOnHandler(post)}> <FaThumbsUp /> {post.likes} </span> {' '}
-                <span onClick={() => this.props.dislikeOnHandler(post)} style={{ marginLeft: "10px" }}> <FaThumbsDown /> {post.dislikes} </span>
+              <div style={{ textAlign: "left", marginLeft: "50px" }}>
+                <span className="pointer" onClick={() => this.likeDislikeOnHandler('like', post)}> <FaThumbsUp /> {post.likes} </span> {' '}
+                <span className="pointer"  onClick={() => this.likeDislikeOnHandler('dislike', post)} style={{ marginLeft: "10px" }}> <FaThumbsDown /> {post.dislikes} </span>
                 <br /><br />
-                {this.props.userInfo._id == post.id && <DeletePost class="btn btn-danger" onDelete={this.onDelete} id={post._id}> Delete Post </DeletePost>}
+                {this.props.userInfo._id == post.id && <DeletePost onDelete={this.onDelete} id={post._id}> Delete Post </DeletePost>}
               </div>
             </Media>
           </Media>
@@ -64,7 +84,7 @@ class ViewPosts extends Component {
   render() {
     return (
       <div>
-        <h4>Posts from friends</h4>
+        <h4>Posts from friends <span className="float-right">Total Posts { this.props.friendPosts.length } </span> </h4>
         {
           this.setPosts()
         }
