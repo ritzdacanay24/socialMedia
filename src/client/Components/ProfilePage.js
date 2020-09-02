1f7daeb77110c76e8be40d90858cd284e041b5ab
Ritz Dacanay <ritzdacanay24@yahoo.com>
2020-09-02 00:27:00 -0700

    Add profile component

--- src/client/Components/ProfilePage.js
+++ src/client/Components/ProfilePage.js
@@ -1,11 +1,10 @@
-import React, {useState, useEffect, useCallback} from 'react';
-import {Form, Col, Row, Button} from 'reactstrap';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Form, Col, Row, Button } from 'reactstrap';
 import axios from 'axios';
 import Navigation from './Navigation'
 
 
 function ProfilePage(props) {
-
 
     let [responseData,
         setResponseData] = useState('');
@@ -14,7 +13,7 @@ function ProfilePage(props) {
 
         axios({
             "method": "GET",
-            "url": "http://localhost:5000/api/users/5f3ffa50c17709b8e44ce571"
+            "url": "http://localhost:5000/api/users/" + props.userInfo._id
         }).then((response) => {
             setResponseData(response.data)
             console.log(response.data)
@@ -22,40 +21,37 @@ function ProfilePage(props) {
             console.log(error)
         })
     }, [])
-    
+
     useEffect(() => {
         fetchData()
     }, [fetchData])
 
-
-  
-
     return (
-        
+
         <div>
-            <Navigation/>
-            <Row id="profileCon">
-                <Col><h1>{responseData.firstName}</h1></Col>
-                <Col>
-            <Button>log data</Button>
-            <Form>
-                <ul id="dataList">
-                    <li>
-                        First Name: {responseData.firstName}
-                    </li>
-                    <li>
-                        Last Name: {responseData.lastName}
-                    </li>
-                    <li>
-                        Email: {responseData.email}
-                    </li>
-                </ul>
-            </Form>
-            <Button>Add More Info</Button>
-            </Col>
-            </Row>
+            <Navigation />
+            <h2 className="text-center text-white">Profile</h2>
+            <div className="container dark-red1" style={{ marginTop: "50px" }}>
+                
+                <div className="row">
+                    <div class="col-md-6 img">
+                        <img src={responseData.profileImage} alt="" class="img-rounded" />
+                    </div>
+                    <div className="col-md-6 details">
+                        <blockquote>
+                            <h5>{responseData.firstName} {responseData.lastName}</h5>
+                            <small><cite title="Source Title">Las Vegas, NV  <i className="icon-map-marker"></i></cite></small>
+                        </blockquote>
+                        <p>
+                            Address goes here <br />
+                                Github account here  <br />
+                                    Created Account on {responseData.timeStamp}
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-       
+
 
     )
 }
